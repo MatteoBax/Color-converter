a133dd19f2bf37562d6ee58ce5848c5d6f6a9fc1
MatteoBax <98881971+MatteoBax@users.noreply.github.com>
2022-10-14 22:32:09 +0200

    Add files via upload

--- src/launcher/converter.cs
+++ src/launcher/converter.cs
@@ -1,10 +1,19 @@
 using System;
 using System.IO;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 class converter{
+    [DllImport("kernel32.dll")]
+    static extern IntPtr GetConsoleWindow();
+    [DllImport("user32.dll")]
+    static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+    const int SW_HIDE = 0;
     static Process process = new Process();
     public static void Main(string[] args)
     {
+	  var handle = GetConsoleWindow();
+        //Hide
+        ShowWindow(handle, SW_HIDE);
 	  string path = @"-jar " + System.AppContext.BaseDirectory + "\\converter.jar";
         process.StartInfo.UseShellExecute = false;
         process.StartInfo.FileName = "java";
